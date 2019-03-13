c26b4fa1674986f1c79077835a4045ea945af639
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2019-03-13 07:23:21 -0400

    Using Namesoace

---
+++ C++/Sams Teach Yourself C++ in One Hour a Day, Eighth Edition/Lesson 2/UsingNamespace.cpp
@@ -0,0 +1,15 @@
+// Preprocessor directive
+#include <iostream>
+
+// Start of the program
+int main()
+{
+    // Tell the compiler what namespace to search in
+    using namespace std;
+
+    /* Write to the screen using std::count */
+    cout << "Hello Adam" << endl;
+
+    // Return a value to the OS
+    return 0;
+}
