de87b83c8a51cead908bfa65d7643d9d42d37538
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2019-03-14 07:06:42 -0400

    Added subtraction and multiplication

--- C++/Sams Teach Yourself C++ in One Hour a Day, Eighth Edition/Lesson 2/UsingFunctions.cpp
+++ C++/Sams Teach Yourself C++ in One Hour a Day, Eighth Edition/Lesson 2/UsingFunctions.cpp
@@ -17,6 +17,8 @@ int DemoConsoleOutput()
 {
     cout << "This is a simple string literal" << endl;
     cout << "Writing number five: " << 5 << endl;
+    cout << "Performing division 10 - 5 = " << 10 - 5 << endl;
+    cout << "Performing division 10 * 5 = " << 10 * 5 << endl;
     cout << "Performing division 10 / 5 = " << 10 / 5 << endl;
     cout << "Pi when approximated is 22 /7 = " << 22 / 7 << endl;
     cout << "Pi is 22 / 7 = " << 22.0 / 7 << endl;
