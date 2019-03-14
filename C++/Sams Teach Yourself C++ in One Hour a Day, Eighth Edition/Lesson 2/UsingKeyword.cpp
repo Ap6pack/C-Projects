350e98cc83b2b1e0b688b0d44142e5ed19751d31
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2019-03-14 04:02:04 -0400

    Example Using Keywords

---
+++ C++/Sams Teach Yourself C++ in One Hour a Day, Eighth Edition/Lesson 2/UsingKeyword.cpp
@@ -0,0 +1,15 @@
+// Preprocessor directive 
+#include <iostream>
+
+// Start of my program
+int main()
+{
+    using std::cout;
+    using std::endl;
+
+    /* Write to the screen using std::cout */
+    cout << "Hello Adam" << endl;
+
+    // Return a value to the OS 
+    return 0;
+}
